219d3af9bde069523225fec79aae8d697231440d
EmmaTheBoar <120807095+EmmaTheBoar@users.noreply.github.com>
2023-08-28 22:31:49 -0500

    Added myself to the output

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.print("Logan Harris\n");
 		System.out.print("Computer Science\n"); 
+		System.out.print("Emma was here too!!!!!\n");
 	}
 }
