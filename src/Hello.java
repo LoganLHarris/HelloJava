b7bf8c0352a41698d9d1eb2e1f884ce9b5550770
Qi <Qi@192.168.1.5>
2023-08-28 19:18:12 -0500

    Added linebreaks

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.print("Logan Harris");
-	    System.out.print("Computer Science");
+		System.out.print("Logan Harris\n");
+	    System.out.print("Computer Science\n");
 	}
 }
